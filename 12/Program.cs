2f468e1ffd1869213020f1cd1cdef1a370c3adf6
Дмитрий Иванов <cel-dmitrij@yandex.ru>
2026-08-24 21:32:29 +0300

    Добавьте файлы проекта.

---
+++ 12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число N > 0: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int s = 0;
+        if (N <= 0)
+        {
+            Console.Write("Ошибка : число должно быть > 0");
+        }
+       else
+        
+        for (int i = 1; i <= N; i++)
+        {
+            s+= 2 * i - 1;
+            Console.WriteLine($"Число {i}, его квадрат: {s}");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число > 0 ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine("Ошибка: число должно быть > 0");
+            }
+            else
+                for (; n <= 10 ; n += 2 + (2 * n - 1))
+                {
+                    Console.WriteLine("Квадрат числа {0} равен {1}", n, n * n);
+                }
+            Console.ReadKey();
+        }
+    }
+}
